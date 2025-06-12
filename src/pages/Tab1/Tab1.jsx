01579a3e34537d2a64d77846b613c08605f503da
hvl2258 <hvl2258@gepplus.co.kr>
2025-06-12 17:24:35 +0900

    2주차 - URLSearchParams -> useSearchParams 변경

--- src/pages/Tab1/Tab1.jsx
+++ src/pages/Tab1/Tab1.jsx
@@ -1,48 +1,50 @@
-import { useEffect, useState } from 'react';
-import Category from '../../components/Tab1/Category';
-import './Tab1.css';
-import { useLocation } from 'react-router';
-import ItemCard from '../../components/Tab1/ItemCard';
+import { useEffect, useState } from "react";
+import Category from "../../components/Tab1/Category";
+import "./Tab1.css";
+import { useLocation, useSearchParams } from "react-router";
+import ItemCard from "../../components/Tab1/ItemCard";
 
 export default function Tab1() {
-  const [loading, setLoading] = useState(true);
-  const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [categories, setCategories] = useState([]);
+	const [products, setProducts] = useState([]);
 
-  const { search } = useLocation();
+	// const { search } = useLocation();
+	// const params = new URLSearchParams(search);
+	// const curGroupCode = params.get("groupCode") || "100000000";
+	const [searchParams] = useSearchParams();
 
-  const params = new URLSearchParams(search);
-  const curGroupCode = params.get('groupCode') || '100000000';
+	const curGroupCode = searchParams.get("groupCode") || "100000000";
 
-  useEffect(() => {
-    setLoading(true); // 요청 시작
+	useEffect(() => {
+		setLoading(true); // 요청 시작
 
-    Promise.all([
-      fetch('/categoryData.json').then((res) => res.json()),
-      fetch('/itemData.json').then((res) => res.json()),
-    ]).then(([categoryRes, productRes]) => {
-      setCategories(categoryRes.categoryData);
-      setProducts(productRes);
+		Promise.all([
+			fetch("/categoryData.json").then((res) => res.json()),
+			fetch("/itemData.json").then((res) => res.json()),
+		]).then(([categoryRes, productRes]) => {
+			setCategories(categoryRes.categoryData);
+			setProducts(productRes);
 
-      setLoading(false);
-    });
-  }, []);
+			setLoading(false);
+		});
+	}, []);
 
-  if (loading) {
-    return <div>로딩 중...</div>;
-  }
+	if (loading) {
+		return <div>로딩 중...</div>;
+	}
 
-  const curProducts = products[curGroupCode] || products[100000000];
+	const curProducts = products[curGroupCode] || products[100000000];
 
-  return (
-    <div className="tab1">
-      <header className="header">베스트</header>
-      <Category categories={categories} curGroupCode={curGroupCode} />
-      <ul className="list__product">
-        {curProducts.map((items) => (
-          <ItemCard products={items} key={items.goodsCode} />
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className="tab1">
+			<header className="header">베스트</header>
+			<Category categories={categories} curGroupCode={curGroupCode} />
+			<ul className="list__product">
+				{curProducts.map((items) => (
+					<ItemCard products={items} key={items.goodsCode} />
+				))}
+			</ul>
+		</div>
+	);
 }
