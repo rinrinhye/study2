29e16650b1398a7ad9f45aa84b7c53f68da7109c
hvl2258 <hvl2258@gepplus.co.kr>
2025-08-27 15:24:50 +0900

    3차 스터디 - 2주차 준비

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,25 @@
-import { NavLink } from "react-router";
+import {NavLink} from "react-router";
 
 const tabs = [
-  { title: "practice-1", params: "/tab1" },
-  { title: "practice-2", params: "/tab2" },
-  { title: "practice-3", params: "/tab3" },
-  { title: "practice-4", params: "/tab4" },
-  { title: "practice-5", params: "/tab5" },
-  { title: "practice-6", params: "/tab6" },
+	{title: "practice-1", params: "/tab1"},
+	{title: "practice-2", params: "/tab2"},
+	{title: "practice-3", params: "/tab3"},
+	{title: "practice-4", params: "/tab4"},
+	{title: "practice-5", params: "/tab5"},
+	{title: "practice-6", params: "/tab6"},
+	{title: "practice-7", params: "/tab7"},
 ];
 
 export default function Header() {
-  return (
-    <header>
-      <nav className="flex items-center justify-center flex-wrap p-2 mb-4">
-        {tabs.map((tab, index) => (
-          <NavLink
-            to={tab.params}
-            key={index}
-            className={({ isActive }) =>
-              `px-8 py-6  hover:text-indigo-500 transition-colors duration-300 ease-in-out ${
-                isActive ? "font-bold text-indigo-500 " : "font-medium"
-              }`
-            }
-          >
-            {tab.title}
-          </NavLink>
-        ))}
-      </nav>
-    </header>
-  );
+	return (
+		<header>
+			<nav className='flex items-center justify-center flex-wrap p-2 mb-4'>
+				{tabs.map((tab, index) => (
+					<NavLink to={tab.params} key={index} className={({isActive}) => `px-8 py-6  hover:text-indigo-500 transition-colors duration-300 ease-in-out ${isActive ? "font-bold text-indigo-500 " : "font-medium"}`}>
+						{tab.title}
+					</NavLink>
+				))}
+			</nav>
+		</header>
+	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router";
 import Tab1 from "./pages/Tab1/Tab1.jsx";
 import Tab2 from "./pages/Tab2/Tab2.jsx";
 import Tab3 from "./pages/Tab3/Tab3.jsx";
@@ -9,6 +9,7 @@ import Tab5 from "./pages/Tab5/Tab5.jsx";
 import Tab6 from "./pages/Tab6/Tab6.jsx";
 import "swiper/css";
 import "./styles/tailwind.css";
+import Tab7 from "./pages/Tab7/Tab7.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -31,26 +32,25 @@ import "./styles/tailwind.css";
 // ]);
 
 const tabs = [
-  { path: "tab1", element: <Tab1 /> },
-  { path: "tab2", element: <Tab2 /> },
-  { path: "tab3", element: <Tab3 /> },
-  { path: "tab4", element: <Tab4 /> },
-  { path: "tab5", element: <Tab5 /> },
-  { path: "tab6", element: <Tab6 /> },
+	{path: "tab1", element: <Tab1 />},
+	{path: "tab2", element: <Tab2 />},
+	{path: "tab3", element: <Tab3 />},
+	{path: "tab4", element: <Tab4 />},
+	{path: "tab5", element: <Tab5 />},
+	{path: "tab6", element: <Tab6 />},
+	{path: "tab7", element: <Tab7 />},
 ];
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      // index → tab1 으로 자동 리디렉션
-      { index: true, element: <Navigate to="tab1" replace /> },
-      ...tabs,
-    ],
-  },
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			// index → tab1 으로 자동 리디렉션
+			{index: true, element: <Navigate to='tab1' replace />},
+			...tabs,
+		],
+	},
 ]);
 
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
