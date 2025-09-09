ce1f216c67f6c0c1ebaa0fda14b5ee628d623e95
hvl2258 <hvl2258@gepplus.co.kr>
2025-09-09 15:57:17 +0900

    3차 스터디 - 3주차 ing

--- src/components/Common/Fallback.jsx
+++ src/components/Common/Fallback.jsx
@@ -2,7 +2,7 @@ export default function Fallback({error}) {
 	return (
 		<div style={{padding: 20, textAlign: "center", color: "red"}}>
 			<h2>⚠️ 오류 발생</h2>
-			<p>{error?.message}</p>
+			{error?.message && <p>{error?.message}</p>}
 			<p>3초 뒤 자동으로 다시 시도합니다...</p>
 		</div>
 	);
